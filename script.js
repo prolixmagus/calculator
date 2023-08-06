3c9a42d77f6bd3f3aeb2940e6a9be372493e1a5c
prolixmagus <jeremypfishman@gmail.com>
2023-08-05 20:26:05 -0700

    Calculator HTML + CSS Create calculator HTML. Style using grid. To do: Finish javascript. Attach event listeners to buttons to display when clicked or keypress.

--- script.js
+++ script.js
@@ -2,34 +2,25 @@ const num1 = 3
 const num2 = 5
 const operator = '+'
 
-// arithmetic functions
+// arithmetic function expressions
 
-function add(num1, num2) {
-    return num1 + num2
-}
-
-function subtract(num1, num2) {
-    return num1 - num2
-}
-
-function multiply(num1, num2) {
-    return num1 * num2
-}
-
-function divide(num1, num2) {
-    return num1 / num2
-}
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
 
 // operate functions
-function operate(firstNumber, operator, secondNumber) {
+function operate(num1, operator, num2) {
     switch (operator) {
         case '+':
             return add(num1, num2);
         case '-':
-            return subtract(num1, num2);
+            return subtract(num1, num2)
         case 'x':
             return multiply(num1, num2);
         case '÷':
             return divide(num1, num2);
-    }  
+        default: 
+            return "Invalid operator.";
+    }
 }
