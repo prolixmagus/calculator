89e19bf531db1a59223d6320f8a3d6811ebb27ca
prolixmagus <jeremypfishman@gmail.com>
2023-08-06 16:41:18 -0700

    Display Create function to add number values to display window on button click.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const num1 = 3
-const num2 = 5
-const operator = '+'
-
+const num1 = null
+const num2 = null
+const operator = null
+let displayValue = ''
 // arithmetic function expressions
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const divide = (num1, num2) => Number((num1 / num2).toFixed(7))
 
 // operate functions
 function operate(num1, operator, num2) {
@@ -24,3 +24,21 @@ function operate(num1, operator, num2) {
             return "Invalid operator.";
     }
 }
+
+function changeDisplay() {
+    const display = document.querySelector('#display');
+    display.textContent = displayValue;
+}
+
+function buttonClick() {
+    const buttons = document.querySelectorAll('button')
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            displayValue += button.value;
+            console.log(displayValue);
+            changeDisplay()
+        })
+    })
+}
+
+buttonClick()
