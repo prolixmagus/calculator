f444521221b832f5fb508e347b42fb7155a4924f
prolixmagus <jeremypfishman@gmail.com>
2023-08-07 18:15:20 -0700

    Core Functionality Calculator is able to chain artihmetic operations. Refactor code to be cleaner and to avoid crashes from null input

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//global variables
 let num1 = null
 let num2 = null
 let operator = ''
 let displayValue = '0'
 let result = ''
 
+//button querySelectors
 const numberButtons = document.querySelectorAll('.num-btn')
 const clearButton = document.querySelector('#clear')
 const operatorButtons = document.querySelectorAll('.operator')
@@ -11,19 +13,12 @@ const decimalButton = document.querySelector('#decimal')
 const equalsButton = document.querySelector('#equals')
 
 // arithmetic function expressions
-
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = function(num1, num2) {
-    if (num2 === 0) {
-        result = 'if only';
-        displayValue = result
-        changeDisplay();
-    } else
-        return num1 / num2
-    }
+const divide = (num1, num2) =>  num1 / num2;
 
+//number display
 function changeDisplay() {
     const display = document.querySelector('#display');
     display.textContent = displayValue;
@@ -39,7 +34,7 @@ function clearDisplay() {
     });
 }
 
-//ask for help here haha (or show nick?)
+//button input (event listeners)
 function inputOperand() {
     numberButtons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -49,14 +44,15 @@ function inputOperand() {
                 }
                 displayValue += button.value;
                 num1 = displayValue
+                console.log(num1)
                 changeDisplay();
-               
             } else {
                 if (displayValue === num1) {
                     displayValue = ''
                 }
                 displayValue += button.value;
                 num2 = displayValue
+                console.log(num2)
                 changeDisplay();
             }
         })
@@ -66,19 +62,41 @@ function inputOperand() {
 function inputOperator() {
     operatorButtons.forEach((button) => {
         button.addEventListener('click', (e) => {
-            operator = button.value;
+            // (3 + 4) +
+            if (num1 !==null && operator !== '' && num2 !== null) {
+                evaluate()
+                operator = button.value
+            } else {
+            operator = button.value
+            }
         });
     });
 }
 
 function equals() {
-    equalsButton.addEventListener('click', () => {
-        result = operate(Number(num1), operator, Number(num2));
-        result = roundLargeNumbers(result)
-        displayValue = result;
-        changeDisplay();  
-    });
+    equalsButton.addEventListener('click', evaluate)
 }
+
+
+function evaluate() {
+        if (num1 !== null && operator !== '' && num2 !== null) {
+            result = operate(Number(num1), operator, Number(num2));
+            if (result === Infinity) {
+                result = '(╯°□°)╯︵ ┻━┻';
+                displayValue = result;
+                changeDisplay(); 
+            }
+            result = roundLargeNumbers(result);
+            displayValue = result;      // display initial result
+            num1 = result               // for chaining operators
+            num2 = null;                // reset the value of num2
+            changeDisplay();  
+        } else {
+            displayValue = '0';
+            changeDisplay();
+        };
+    };
+
 function roundLargeNumbers(){
     if (result.toString().includes('.')) {
         if (result.toString().split('.')[1].length > 4) {
@@ -88,7 +106,6 @@ function roundLargeNumbers(){
         return result;
       }
 
-// operate functions
 function operate(num1, operator, num2) {
     switch (operator) {
         case '+':
@@ -102,9 +119,12 @@ function operate(num1, operator, num2) {
     }
 }
 
+function calculate() {
+    inputOperand()
+    inputOperator()
+    clearDisplay();
+    equals()
+}
 
-
-inputOperand()
-inputOperator()
-clearDisplay();
-equals()
+//all together now
+calculate()
